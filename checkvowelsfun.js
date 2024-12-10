677e20db9e0b3d3a58ef845a3b1fd335e4e6a15d
shreeya <pshreeya050@gmail.com>
2024-12-10 14:52:40 +0530

    changes

--- checkvowelsfun.js
+++ checkvowelsfun.js
@@ -1,14 +1,11 @@
-let arr=['a','e','b','c','d','y','j','i'];
+let arr=['a','e','b','c','d','y','j','i','o','u'];
 function vowel() {
-    let count=0;
     for(let j=0;j<arr.length;j++){
         if(arr[j]=='a' || arr[j]=='e' || arr[j]=='i' || arr[j]=='o' || arr[j]=='u'){
             console.log('vowels prsent '+ arr[j]);
-            count ++;
+            
         }
-    }
-    console.log('total count of vowel is '+count);
-
+    }   
 }
 
 vowel();
